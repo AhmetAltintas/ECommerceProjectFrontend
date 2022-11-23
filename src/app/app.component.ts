1a863a817be34f833fe742e1c76c81902f17c7be
Ahmet ALTINTAŞ <ahmtaltnts35@gmail.com>
2022-11-23 23:05:21 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FinalProject';
+  title: string = 'FinalProject';
+  user: string = "Ahmet ALTINTAŞ";
+  
 }
